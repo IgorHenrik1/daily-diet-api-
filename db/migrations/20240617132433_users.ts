c041aba6de356b76cb8993f994041e3b73dba633
Igor <igor.freitas@gruponsc.com.br>
2024-06-17 10:33:42 -0300

    feat:  migration use

--- db/migrations/20240617132433_users.ts
+++ db/migrations/20240617132433_users.ts
@@ -1,10 +1,16 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name')
+    table.string('email')
+    table.string('password')
+    table.string('user_id')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-
